ad0d499bbb7801dff678e2fcb434109e34078596
DasDuneTech <dasdunetech@gmail.com>
2022-05-24 16:26:49 +0700

    updates

---
+++ app-sheets.js
@@ -0,0 +1,20 @@
+const {accessToken} = require(`./oauth2GoogleFunctions`)
+const fetch = require('node-fetch');
+
+sheetRange = async() => {
+
+    //get an access token
+    let token = await accessToken()
+    console.log(token)
+
+    //Get the column values
+    url = `https://sheets.googleapis.com/v4/spreadsheets/1TIQfrcPM15l_4NIjDOz7MMe3EtHfIR8_aST4YD-PEY4/values/A1%3AJ9`
+
+    res = await fetch(`${url}`, {headers: {Authorization: 'Bearer ' + token}});
+    let range  = await res.json()
+
+    return range
+
+}
+
+sheetRange()
