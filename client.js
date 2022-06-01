239d3e11ab586498a0e53185ab95f19fb2556577
DasDuneTech <dasdunetech@gmail.com>
2022-06-01 14:11:49 +0700

    updates

---
+++ client.js
@@ -0,0 +1,73 @@
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+
+
+
+
+
+
+
+//http get request 
+const hello = async() => {
+
+    const res = await fetch(`http://localhost:8080/hello`)
+    let res2 = await res.text()
+
+    console.log(res2)
+}
+
+//http get request with parameters
+const hello2 = async() => {
+
+    const res = await fetch(`http://localhost:8080/hello2/?firstName=John&lastName=Doe`)
+    let res2 = await res.text()
+
+    console.log(res2)
+
+}
+
+
+//http post request with JSON
+const postReqJSON= async() => {
+
+    let data = {firstName: "John", lastName: "Doe"}
+    let url = `http://localhost:8080/hello`
+
+    let res = await fetch(url, 
+        {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+            },
+        body: JSON.stringify(data)
+    });
+    
+    let res2  = await res.text()
+
+    console.log(res2)
+}
+
+//http post request with FormData
+const postReqFormData= async() => {
+
+    const formData2 = new FormData();
+    formData2.append('firstName', 'John');
+    formData2.append('lasttName', 'Doe');
+
+    let url = `http://localhost:8080/hello3`
+
+    let res = await fetch(url, 
+        {
+        method: 'POST',
+        body: formData2
+    });
+    
+    let res2  = await res.text()
+
+    console.log(res2)
+
+}
+
+
+
+postReqFormData()
