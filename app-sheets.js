b1dc52176540d2e16b040316d707b92621417eb8
DasDuneTech <dasdunetech@gmail.com>
2022-06-01 14:04:34 +0700

    updates

--- app-sheets.js
+++ app-sheets.js
@@ -1,4 +1,3 @@
-
 // * Basic HTTP server
 const express = require('express');
 const app = express();
@@ -9,54 +8,157 @@ app.listen(port, () => {
     console.log(`Server listening at port ${port}`);
 });
 
-const {accessToken, getFilesList, getSheetsList, getInfo, update, batchUpdate} = require(`./GoogleLib`)
+// JSON parsing
+app.use(express.json({ limit: '50mb' }));
+
+// FormData parsing
+const multer = require('multer');
+const upload = multer();
+
+// for parsing multipart/form-data
+app.use(upload.array()); 
+
+//Google Sheets Functions library
+const {codeRequest, refreshToken, accessToken, getFilesList, getSpreadsheetsList, getSheetsList, getInfo, update, batchUpdate} = require(`./GoogleLib`)
 
 
+//http get response 
+app.get('/hello', async (req, res) => res.send(`Hello whoever you are`))
+
+
+
+//http get response with parameters
+app.get('/hello2', async (req, res) => {
+
+   firstName = req.query.firstName
+   lastName = req.query.lastName
+ 
+   res.send(`Hello ${firstName} ${lastName}`)
+
+})
+
+//http post response with JSON / formData body object
+app.post('/hello3', async (req, res) => {
+
+   try{
+       const {firstName, lastName} = req.body
+       res.send(`Hello ${firstName} ${lastName}`)
+   }
+   catch(err){
+       res.send(`error :: ${err.message}`)
+       return
+   }
+
+})
+
+
+//redirection to Google Auth to get an authorization code
+app.get('/oauth2Google', async(req, res) => {
+
+    let url = await codeRequest()
+    res.redirect(url);
+
+});
+
+
+
+// redirect URI endpoint to receive the authorization code
+app.get('/oauth2GoogleCallback', async(req, res) => {
+
+    let code = req.query.code
+
+    let info =`oauth2GoogleCallback ::\n\t`
+    for (const [key, val] of Object.entries(req.query)) info += `${key}: ${val}\n\t`
+    console.log(info)
+
+    let tokenInfo = await refreshToken({code:code})
+
+    if (tokenInfo.refresh_token === undefined) {
+        info =`oauth2GoogleCallback ::\n\t`
+        info += `error : Cannot exchange the access code for a refresh token`
+        console.log(info)
+        res.send(`error : Cannot exchange the access code for a refresh token`);
+    } 
+    else {
+        fs.writeFileSync('oauth2GoogleRefreshToken.txt', tokenInfo.refresh_token)
+        res.send(`thanks user to trust our app.`);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//get files details list from Google Sheets
 app.get('/getFilesList', async (req, res) => {
 
    await accessToken() 
    let info =  await getFilesList()
    console.log(info)
+   res.send(info)
 
 })
 
-
-
-app.get('/getSheetsList', async (req, res) => {
+//get spreadsheets details list from Google Sheets
+app.get('/getSpreadsheetsList', async (req, res) => {
 
     await accessToken() 
-    let info =  await getSheetsList()
+    let info =  await getSpreadsheetsList()
     console.log(info)
+    res.send(info)
  
  })
 
+//get sheets details for a given spreadsheet
+ app.get('/getSheetsList', async (req, res) => {
 
+   await accessToken() 
+   let info =  await getSheetsList(`Library`)
+   console.log(info.sheets)
+   res.send(info.sheets)
 
+})
+
+//get sheet info for a given sheet (values or formulas)
  app.get('/getInfo', async (req, res) => {
 
     await accessToken() 
-    let sheetsInfo = {sheetsName:`Library`, sheetName:`Library`, isFormulas:true}
-    let info =  await getInfo(sheetsInfo)
+    let spreadsheetInfo = {spreadsheetName:`Library`, sheetName:`Library`, isFormulas:true}
+    let info =  await getInfo(spreadsheetInfo)
     console.log(info)
- 
+    res.send(info)
+
  })
 
-
+//update a range for a given sheet 
  app.get('/update', async (req, res) => {
 
     await accessToken() 
     let payload = [[777],["=HYPERLINK(\"https://dasdunetech.com/library/Google.html\",\"Google API\")"]]
     let range = `B3:B4`
-    let sheetsInfo = {sheetsName:`Library`, sheetName:`Library`, range:range, payload:payload}
-    let info = await update(sheetsInfo)
-    return(info)
+    let spreadsheetInfo = {spreadsheetName:`Library`, sheetName:`Library`, range:range, payload:payload}
+    let info = await update(spreadsheetInfo)
+    console.log(info)
+    res.send(info)
+
  })
 
+ //update multiple ranges for multiples sheets for a given spreadsheet
  app.get('/batchUpdate', async (req, res) => {
 
     await accessToken() 
     let payload = [["Library!C4:C4", 888],["Sheet2!D4:D4", 889],["Library!E4:E4", 887]]
     let sheetsInfo = {sheetsName:`Library`, payload:payload}
     let info = await batchUpdate(sheetsInfo)
-    return(info)
+    console.log(info)
+    res.send(info)
+
 })
